d792d3a2a48ab2559fe5f16c130b628ad92e8b7a
David <rozenblum3@mail.tau.ac.il>
2025-03-09 09:08:30 +0000

    Added framework for module

--- symnmfmodule.c
+++ symnmfmodule.c
@@ -7,3 +7,53 @@ symnmf,sym,ddg,norm for Python */
 
 /* Function declarations */
 
+
+
+
+static PyObject* symnmf(PyObject* self, PyObject* args) {
+    // TODO David
+    printf("WIP\n");
+    Py_RETURN_NONE;
+}
+
+static PyObject* sym(PyObject* self, PyObject* args) {
+    // TODO David
+    printf("WIP\n");
+    Py_RETURN_NONE;
+}
+
+static PyObject* ddg(PyObject* self, PyObject* args) {
+    // TODO David
+    printf("WIP\n");
+    Py_RETURN_NONE;
+}
+
+static PyObject* norm(PyObject* self, PyObject* args) {
+    // TODO David
+    printf("WIP\n");
+    Py_RETURN_NONE;
+}
+
+static PyMethodDef symnmfmethods[] = {
+    {"symnmf", symnmf, METH_VARARGS, "Performs SymNMF on a matrix."},
+    {"sym", sym, METH_VARARGS, "Performs Sym on a matrix."},
+    {"ddg", ddg, METH_VARARGS, "Performs DDG on a matrix."},
+    {"norm", norm, METH_VARARGS, "Performs Norm on a matrix."},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef symnmfmodule = {
+    PyModuleDef_HEAD_INIT,
+    "symnmfmodule",
+    "A module that performs SymNMF on a matrix.",
+    -1,
+    symnmfmethods
+};
+
+PyMODINIT_FUNC PyInit_symnmfmodule(void) {
+    PyObject* m = PyModule_Create(&symnmfmodule);
+    if (m == NULL) {
+        return NULL;
+    }
+    return m;
+}
