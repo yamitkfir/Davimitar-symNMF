708d72bf25925cb9fa1d7e8d2c5e9726c06ba4f3
Saar DR <saard@mail.tau.ac.il>
2025-03-10 12:41:24 +0000

    Updated function declarations

--- symnmfalgo.h
+++ symnmfalgo.h
@@ -6,18 +6,19 @@
 #include <math.h>
 
 /* Function declarations */
-void free_matrix(double** M);
-void exit_with_error();
-double matrix_mult_cell(double** A, double** B, int i, int j);
 double squared_euclidean_dist(double* point1, double* point2, int dimension);
-double frobenius_norm(double** A, double** B);
-double** get_column(double** M, int j);
-double** optimizing_H(double** H, double** W);
+double** get_column(double** M, int rows_num, int j);
+double** optimizing_H(double** H, int rows_num, int cols_num, double** W);
 int update_H(double** W, double** H, double** new_H, int n, int k);
 double** similarity_matrix(double** datapoints, int n, int d);
 double** diagonal_degree_matrix(double** A, int n);
-double** multiplyMatrix(double** matrixA, double** matrixB, int m, int n, int k); // A - m x n, B - n x k
-double** similarity_matrix(double** datapoints, int n, int d);
-double** normalized_similarity_matrix(double** sim_matrix, int n);
+
+/* Helper functions */
+double sq_euclidian_norm(double* point1, double* point2, int dimension);
+double frobenius_norm(double** A, int rows_num, int cols_num, double** B);
+void free_matrix(double** M, int len);
+double** multiplyMatrix(double** matrixA, double** matrixB, int m, int n, int k); /* A - m x n, B - n x k */
+double matrix_mult_cell(double** A, int A_cols_num ,double** B, int i, int j);
+void exit_with_error();
 
 #endif /* SYMNMFALGO_H */
