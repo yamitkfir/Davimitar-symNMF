d010eba79c458100054eb6637153218d777fde52
Saar DR <saard@mail.tau.ac.il>
2025-03-14 17:52:32 +0000

    Shortened every function Yamit sent in the group to under 40 lines - It didn't upload right the first time

--- symnmf.c
+++ symnmf.c
@@ -20,7 +20,6 @@
 double squared_euclidean_dist(double* point1, double* point2, int dimension);
 double** get_column(double** M, int rows_num, int j);
 double** optimizing_H(double** H, int rows_num, int cols_num, double** W);
-void update_H_cell(double **W, double **H, double **new_H, double **HtH_col, int n, int k, int i, int j);
 int update_H(double** W, double** H, double** new_H, int n, int k);
 double** similarity_matrix(double** datapoints, int n, int d);
 double** diagonal_degree_matrix(double** A, int n);
@@ -30,10 +29,12 @@ double **read_data(const char *filename, int *n, int *d);
 void print_matrix(double **matrix, int rows, int cols);
 
 /* Helper functions */
+double **create_points_matrix(FILE *fp, char line[], int *n, int *d);
 double frobenius_norm(double** A, int rows_num, int cols_num, double** B);
 void free_matrix(double** M, int len);
 double** multiply_matrix(double** matrixA, double** matrixB, int m, int n, int k); /* A - m x n, B - n x k */
 double matrix_mult_cell(double** A, int A_cols_num ,double** B, int i, int j);
+void update_H_cell(double **W, double **H, double **new_H, double **HtH_col, int n, int k, int i, int j);
 void exit_with_error();
 
 
